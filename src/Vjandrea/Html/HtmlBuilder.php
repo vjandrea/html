4937e61b23d9f5b9e930ba4b6f747c83abc1b322
Andrea Bergamasco <andrea@mbp-di-andrea.station>
2014-07-31 22:45:48 +0200

    Added html_lang method, renamed metacharset as meta_charset

--- src/Vjandrea/Html/HtmlBuilder.php
+++ src/Vjandrea/Html/HtmlBuilder.php
@@ -12,8 +12,9 @@ class HtmlBuilder extends \Illuminate\Html\HtmlBuilder
     * @return string
     */
     public function favicon($href = '', $type = 'image/png') {
-        return $href ? '<link rel="icon" type="'. $type . '" href="'. $href . '">' : '';
+        return $href ? '<link rel="icon" type="' . $type . '" href="'. $href . '">' : '';
     }
+    
     
     /**
     * Returns a meta charset tag
@@ -21,9 +22,18 @@ class HtmlBuilder extends \Illuminate\Html\HtmlBuilder
     * @param string $charset default 'utf-8'
     * @return string
     */
-    public function metacharset($charset = 'utf-8') {
-        return '<meta charset="'.$charset.'">';
+    public function meta_charset($charset = 'utf-8') {
+        return '<meta charset="' . $charset . '">';
     }
     
     
+  /**
+  * Returns an html with lang parameter
+  *
+  * @param string $lang default 'en'
+  * @return string
+  */
+  public function html_lang($lang = 'en') {
+    return '<html lang="' . $lang . '">';
+  }
 }
