26f1dbc4b7a8d2df53dd543c67b90d476a80e91b
Andrea Bergamasco <andrea@mbp-di-andrea.station>
2014-07-31 22:28:03 +0200

    Added meta charset method

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ Html extension
 ==============
 
 This packages adds some missing methods to Illuminate/Html
+
+Methods
+-------
+
+*favicon($href, $type)*
--- src/Vjandrea/Html/HtmlBuilder.php
+++ src/Vjandrea/Html/HtmlBuilder.php
@@ -5,14 +5,24 @@ use Illuminate\Http\Request;
 class HtmlBuilder extends \Illuminate\Html\HtmlBuilder
 {
     /**
-    * Returns a favicon link for specified url
+    * Returns a favicon link to the specified image href
     * 
-    * @param $href string default ''
-    * @param $type string default 'image/png'  
+    * @param string $href default ''
+    * @param string $type default 'image/png'  
     * @return string
     */
     public function favicon($href = '', $type = 'image/png') {
         return $href ? '<link rel="icon" type="'. $type . '" href="'. $href . '">' : '';
+    }
+    
+    /**
+    * Returns a meta charset tag
+    * 
+    * @param string $charset default 'utf-8'
+    * @return string
+    */
+    public function charset($charset = 'utf-8') {
+        return '<meta charset="'.$charset.'">';
     }
     
     
